7d3006de50e6ca2db0680b0b294b36481d7901b5
ryujin_sama <51910093+ryujin8sama@users.noreply.github.com>
2023-06-01 12:49:21 +0200

    Initialisation de la SAE

---
+++ SAE JAVA/src/TestArdoise.java
@@ -0,0 +1,16 @@
+import ardoise.Ardoise;
+import ardoise.PointPlan;
+
+public class TestArdoise {
+
+         public static void main(String []args ){
+
+             Ardoise ardoise = new Ardoise();
+             PointPlan p1 = new PointPlan(50,50);
+
+             ardoise.test();
+
+
+             }
+ }
+
